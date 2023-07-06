cd398b245bcb6163ca3f24626cb624bbcedb0288
TanishqSwami03 <tanishqrajswami@gmail.com>
2023-07-06 23:57:48 +0530

    Next commit (Progress in building the app)

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+
+    return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
