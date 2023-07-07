042e0970c68e2f99a6c4563e0e60bbe4412429a2
TanishqSwami03 <tanishqrajswami@gmail.com>
2023-07-07 22:11:48 +0530

    Next Commit

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
@@ -9,13 +9,21 @@ def home(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+
         # Authenticate
         user = authenticate(request, username=username, password=password)
-
-    return render(request, 'home.html', {})
-
-def login_user(request):
-    pass
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You are successfully logged in.")
+            return redirect('home')
+        else:
+            messages.success(request,"There was an error logging in. Please try again.")
+            return redirect('home')
+        
+    else:
+        return render(request, 'home.html', {})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out !!")
+    return redirect('home')
